b22a3d486af0fc63e8f3bcbcea7d273a12f505bc
zzho0 <rkdwjsgh0@mxlab.kr>
2025-10-26 11:36:21 +0900

    feat:deploy

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
           <App />
         </BrowserRouter>
       </ThemeProvider>
